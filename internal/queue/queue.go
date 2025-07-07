5051c54a754a0bf97e2cbcb5d03f2876e1505b31
Emanuel Forlin <emanuel.forlin@crombie.dev>
2025-07-07 15:29:33 -0300

    feat: implement queue ds

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,9 +1,17 @@
 package queue
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 const (
-	DefaultQueueSize = 100
+	DefaultQueueCapacity    = 100 // default initial capacity of the message queue
+	DefaultMaxQueueCapacity = 200 // maximum capacity of the message queue
+
+	// Error messages
+	EmptyQueueError = "queue is empty" // error message when trying to dequeue from an empty queue
+	FullQueueError  = "queue is full"  // error message when trying to enqueue to a full queue
 )
 
 type Message struct {
@@ -18,6 +26,32 @@ type MessageQueue struct {
 
 func NewMessageQueue() *MessageQueue {
 	return &MessageQueue{
-		queue: make([]Message, 0, DefaultQueueSize),
+		mu:    sync.Mutex{},
+		queue: make([]Message, 0, DefaultQueueCapacity),
 	}
+}
+
+// Enqueue adds a message to the queue.
+func (mq *MessageQueue) Enqueue(msg Message) error {
+	mq.mu.Lock()
+	defer mq.mu.Unlock()
+
+	if len(mq.queue) >= DefaultMaxQueueCapacity {
+		return errors.New(FullQueueError)
+	}
+
+	mq.queue = append(mq.queue, msg)
+	return nil
+}
+
+// Dequeue removes and returns the first (oldest) message from the queue.
+func (mq *MessageQueue) Dequeue() (Message, error) {
+	mq.mu.Lock()
+	defer mq.mu.Unlock()
+	if len(mq.queue) == 0 {
+		return Message{}, errors.New(EmptyQueueError)
+	}
+	msg := mq.queue[0]
+	mq.queue = mq.queue[1:]
+	return msg, nil
 }
